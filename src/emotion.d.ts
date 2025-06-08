d6255979d59c6db79b53d191b2a25cd92c07e7d1
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-06-07 19:16:13 -0700

    lint: disable no-empty-object-type for theme extends apptheme

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -3,5 +3,6 @@ import { AppTheme } from "./styles/theme";
 
 // https://emotion.sh/docs/typescript#define-a-theme
 declare module "@emotion/react" {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     export interface Theme extends AppTheme {}
 }
